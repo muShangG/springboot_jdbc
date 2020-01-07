104e37a7d14af63a86149b2ea0f67eae3b03c042
gaos <1317190298@qq.com>
2020-01-07 21:52:28 +0800

    qqq

--- src/main/java/com/example/springboot_jdbc/config/DruidConfig.java
+++ src/main/java/com/example/springboot_jdbc/config/DruidConfig.java
@@ -2,8 +2,10 @@ package com.example.springboot_jdbc.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
 import com.sun.org.apache.bcel.internal.generic.NEW;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +13,7 @@ import org.springframework.context.annotation.Configuration;
 import javax.servlet.FilterRegistration;
 import javax.servlet.ServletRegistration;
 import javax.sql.DataSource;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -29,12 +32,19 @@ public class DruidConfig {
         stringStringMap.put("loginUsername","admin");
         stringStringMap.put("loginPassword","123");
         stringStringMap.put("allow","");
-        stringStringMap.put("deny","");
+        stringStringMap.put("deny","192.168.1.6");
         bean.setInitParameters(stringStringMap);
         return bean;
     }
-  /*  public FilterRegistration  webfilter(){
-
-    }*/
+    public FilterRegistrationBean webfilter(){
+        FilterRegistrationBean bean=new FilterRegistrationBean();
+        bean.setFilter(new WebStatFilter());
+        Map<String,String> stringMap=new HashMap<>();
+        bean.setInitParameters(stringMap);
+        stringMap.put("exclusions","*.js,*.css,/druid/*");
+        bean.setInitParameters(stringMap);
+        bean.setUrlPatterns(Arrays.asList("/*"));
+        return bean;
+    }
 
 }
